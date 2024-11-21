eb71199d60145fa0e117d2cf9ebd004075575d79
Natallia Samoilik <n.samoilik@sportradar.com>
2024-11-21 14:38:45 +0100

    cookie btn fixed- added if-else

--- tests/UI/google-test.spec.js
+++ tests/UI/google-test.spec.js
@@ -14,8 +14,12 @@ test('Exercise 3 - Google Playwright getting started with geolocation (Wroclaw)'
   await page.goto('https://www.google.pl/?hl=pl');
 
   const cookieBtn = await page.locator("#L2AGLb");
-  await cookieBtn.click();
-
+  if(await cookieBtn.isVisible()){
+  await cookieBtn.click({timeout: 5000 });
+  }else {
+    console.log('Cookie button not visible');
+  }
+  
   const searchBar = await page.getByRole('textbox', { name: 'Search' });
   await searchBar.fill('Playwright getting started');
 
